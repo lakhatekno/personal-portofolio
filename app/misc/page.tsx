df95214c25537e20304c01504b071160a7cd7ef5
muhammad-islakha <lakhatekno2514@gmail.com>
2025-08-15 20:56:05 +0700

    Misc. Create dummy misc page

---
+++ app/misc/page.tsx
@@ -0,0 +1,10 @@
+import { GiCrane } from "react-icons/gi";
+
+export default function Misc() {
+  return (
+    <div className="min-h-svh flex flex-col items-center justify-center">
+      <GiCrane className="text-amber-400 scale-[10] mb-20" />
+      <h2 className="text-5xl text-slate-100 font-extrabold">Under Construction</h2>
+    </div>
+  )
+}
